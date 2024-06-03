249081e6a120ce4a1fe499de6947b8dc424a55dc
Jhan-Paul11 <jhanpaul.bejarano@genstudents.org>
2024-06-03 12:57:20 -0500

    nav and footer

--- front/Assets/app/headeryfooter.js
+++ front/Assets/app/headeryfooter.js
@@ -3,50 +3,62 @@ const footer = document.querySelector('footer');
 
 
 header.innerHTML = `
-<nav class="nav-bar">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<nav>
+<div class="contenedor">
+<div id="barra_nav" class="nav-bar ">
       <a href="#" id="logo">SAMAY</a>
      <div class="texto_menu">
-      <ul >
-        <li><a href="#">INICIO</a></li>
-        <li><a href="#">PRODUCTOS</a></li>
+      <ul>
+        <li><a href="./inicio.html">INICIO</a></li>
+        <li><a href="./Productos.html">PRODUCTOS</a></li>
         <li><a href="#">BLOG</a></li>
-        <li><a href="#">CONTACTO</a></li>
+        <li><a href="./Productos.html">CONTACTO</a></li>
       </ul>      
     </div>
       <div class="icon">
-        <a href="#"><img src="./images/iconoUsuario.jpg" alt="usuario"></a>
-        <a href="#"><img src="./images/carrito.jpg" alt="carrito"></a>
-        <a href="javascript:void(0);" id="menuHamburger"><img src="./images/menuHamburger.jpg" alt="menu"></a>
+        <a href="#"><i class="fa-solid fa-user iconos_header" style="color: #ffffff;"></i></a>
+        <a href="#"><i class="fa-solid fa-cart-shopping iconos_header" style="color: #ffffff;"></i></a>
+        <a href="javascript:void(0);" id="menuHamburger"><i class="fa-solid fa-bars iconos_header" style="color: #ffffff;"></i></a>
       </div>
       <ul class="menuBurger close" id="menuList">
-        <li><a href="#" class="menuItem" id="inicio">Inicio</a></li>
+        <li><a href="./inicio.html" class="menuItem" id="inicio">Inicio</a></li>
         <li ><a href="#" class="menuItem ">Nosotros</a></li>
-        <li ><a href="#" class="menuItem ">Productos</a></li>
+        <li ><a href="./Productos.html" class="menuItem ">Productos</a></li>
         <li ><a href="#" class="menuItem ">Blog</a></li>
-        <li ><a href="#" class="menuItem ">Contactos</a></li>
+        <li ><a href="./Productos.html" class="menuItem ">Contactos</a></li>
       </ul>
-    </nav>
+    </div>
+    </div>
+</nav>    
 `;
 
 footer.innerHTML = `
+<div class="footerTest">
 <div class="contenedor-fotter">
+<img src="./front/images/logo_samay.jpg" alt="logo samay">
 <ul>
-  <li><a href="#">Inicio</a></li>
+  <li><a href="./inicio.html">Inicio</a></li>
   <li><a href="#">Nosotros</a></li>
-  <li><a href="#">Productos</a></li>
+  <li><a href="./Productos.html">Productos</a></li>
   <li><a href="#">Blog</a></li>
-  <li><a href="#">Contacto</a></li>
+  <li><a href="./Productos.html">Contacto</a></li>
 </ul>
-<img src="./images/logo_samay.jpg" alt="">
+
 <div class="contenedor-iconos">
-  <i class="fa-brands fa-facebook fa-lg iconos" style="color: #000000;"></i>
-  <i class="fa-brands fa-instagram fa-lg iconos" style="color: #000000;"></i>
-  <i class="fa-brands fa-whatsapp fa-lg iconos" style="color: #000000;"></i>
-  <i class="fa-solid fa-location-dot fa-lg iconos" style="color: #000000;"></i>
+  <a href="#"><i class="fa-brands fa-facebook fa-lg iconos" style="color: #000000;"></i></a>
+  <a href="#"><i class="fa-brands fa-instagram fa-lg iconos" style="color: #000000;"></i></a>
+  <a href="#"><i class="fa-brands fa-whatsapp fa-lg iconos" style="color: #000000;"></i></a>
+  <a href="#"><i class="fa-solid fa-location-dot fa-lg iconos" style="color: #000000;"></i></a>
 </div>
 </div>
 <div class="Copyright-j">
-<p>Copyright © 2024 Todos los derechos reservados.</p>
-<p><b>Legal | Privacidad | Productos</b></p>
+  <p id="derechos_reservados">Copyright © 2024 Todos los derechos reservados.</p>
+  <div class="links_footer">
+  <a href="#"><p class="texto_copy"><b>Legal |</b></p></a>
+  <a href="#"><p class="texto_copy"><b> Privacidad |</b></p></a>
+  <a href="./Productos.html"><p class="texto_copy"><b> Productos</b></p></a>
+  </div>
 </div>
+</div> 
 `;
